6513f22f86deb2fd271462f5aedda32747a46309
ilimamasab <88340172+ilimamasab@users.noreply.github.com>
2021-08-05 19:12:28 +0800

    update ver 2

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".opt1").click(function(){
+    $(".scroll-alert-msg").show();
+  });
+  $(".opt2").click(function(){
+    $(".scroll-alert-msg").show();
+  });
+  $(".opt3").click(function(){
+    $(".scroll-alert-msg").show();
+  });
+  $(".opt4").click(function(){
+    $(".scroll-alert-msg").show();
+  });
+});
